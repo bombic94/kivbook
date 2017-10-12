9bd66d8cde2cdc3e8666f67f6a4e227c9b133ea8
bohmannd <bohmannd@gmail.com>
2017-10-12 18:37:32 +0200

    Ajax notifications enabled (so far only About.jsp)

---
+++ src/main/java/zcu/pia/bohmannd/controller/AjaxController.java
@@ -0,0 +1,52 @@
+package zcu.pia.bohmannd.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import zcu.pia.bohmannd.model.User;
+import zcu.pia.bohmannd.service.ChatService;
+import zcu.pia.bohmannd.service.FriendshipService;
+import zcu.pia.bohmannd.service.UserService;
+
+@Controller
+public class AjaxController {
+
+	@Autowired
+    private UserService userService;
+	
+	@Autowired
+    private ChatService chatService;
+	
+	@Autowired
+    private FriendshipService friendshipService;
+	
+	final Logger logger = Logger.getLogger(AjaxController.class);
+	
+	@RequestMapping(value = "/ajaxNotif", method = RequestMethod.GET)
+	@ResponseBody
+    public List<Integer> addItems(HttpSession session, HttpServletResponse response) {
+		User user = userService.getUserByUsername(session.getAttribute("USER").toString());
+		List<Integer> notif = new ArrayList<Integer>();
+		if (user != null) {			
+			notif.add(chatService.listUnreadChatByUser(user).size());
+			notif.add(friendshipService.listPendingFriendshipByUser(user).size());
+			response.setContentType("application/json");
+			logger.info("Ajax notifications - Chats: " + notif.get(0) + ", Friendships: " + notif.get(1));			        
+		} else {
+			notif.add(0);
+			notif.add(0);
+		}
+			
+		return notif;
+    }
+}
