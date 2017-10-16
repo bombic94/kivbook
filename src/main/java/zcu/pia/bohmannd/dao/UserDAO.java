04f8dffc87d74b656ffdcd4a1f2007832f2cb07f
bohmannd <bohmannd@gmail.com>
2017-10-16 21:29:13 +0200

    All services and DAOs commented.

--- src/main/java/zcu/pia/bohmannd/dao/UserDAO.java
+++ src/main/java/zcu/pia/bohmannd/dao/UserDAO.java
@@ -5,15 +5,42 @@ import zcu.pia.bohmannd.model.User;
 
 public interface UserDAO extends AbstractDAO<User> {
 
+	/**
+	 * Get user by given username
+	 * @param username String value of username
+	 * @return User
+	 */
 	User getByUsername(String username);
 
+	/**
+	 * Update user settings (name, dateofbirth, gender)
+	 * @param user User to be updated
+	 */
 	void updateSettings(User user);
 
+	/**
+	 * Update user profile picture
+	 * @param user User to be updated
+	 */
 	void updatePhoto(User user);
 
+	/**
+	 * Update user password
+	 * @param user User to be updated
+	 */
 	void updatePassword(User user);
 
+	/**
+	 * Set active chat for given user
+	 * @param user User whose chat setting active
+	 * @param chat Chat to set active
+	 */
 	void setActiveChat(User user, Chat chat);
 
+	/**
+	 * Get active chat for given user
+	 * @param user User whose chat getting
+	 * @return Chat
+	 */
 	Chat getActiveChat(User user);
 }
--- src/main/java/zcu/pia/bohmannd/utils/EmailService.java
+++ src/main/java/zcu/pia/bohmannd/utils/EmailService.java
@@ -4,5 +4,9 @@ import zcu.pia.bohmannd.model.User;
 
 public interface EmailService {
 
+	/**
+	 * Send mail to given user provided mail
+	 * @param user User to mail
+	 */
 	public void sendMail(User user);
 }
